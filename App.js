d4ff6c25e6ea1d79d0561d6324f204987e79f240
Luis Hernández <l.hernandez@nexus.com.do>
2020-11-18 16:40:24 -0400

    improvised splashScreen

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
-import React, {useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 import NavigationByStack from './routes/stackNav'
+import { View, Image } from 'react-native'
 
 export default function App() {
+
+  const [splash, setSplash] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setSplash(false);
+    }, 1000);
+  }, []);
+
   return (
-    <NavigationByStack  />
+    splash ?
+      <Image style={{ height: '100%', width: '100%'}} source={require('./assets/splash1.png')} />
+      : <NavigationByStack />
   );
 
 
